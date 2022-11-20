9acb7b4de287542789e58939930ac7acd64c8d13
verrelio.cr <verrelioragna@gmail.com>
2022-11-21 01:34:23 +0700

    [Verrel] create view for login

---
+++ ReqRes/Modules/Login/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  ReqRes
+//
+//  Created by Verrelio C. Rizky on 21/11/22.
+//
+
+import UIKit
+
+class LoginView: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
