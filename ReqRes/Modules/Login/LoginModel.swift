e6450b8ae437b75bfdf9132b9b9ffeeaf4b02b55
verrelio.cr <verrelioragna@gmail.com>
2022-11-21 01:20:19 +0700

    [Verrel] add login model

---
+++ ReqRes/Modules/Login/LoginModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginModel.swift
+//  ReqRes
+//
+//  Created by Verrelio C. Rizky on 21/11/22.
+//
+
+import Foundation
+
+class LoginModel {
+    var email: String = ""
+    var password: String = ""
+}
